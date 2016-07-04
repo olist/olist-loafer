218747a5cd3a35e71c61a97d0ceccd9f301268ca
Luiz Menezes <luiz.menezesf@gmail.com>
2016-07-04 09:56:21 -0300

    update setup.py to get loafer version from CHANGES.rst

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 import codecs
 import os.path
+import re
 from setuptools import setup, find_packages, Command
 
 
@@ -13,6 +14,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, 'loafer/__init__.py'), encoding='utf-8') as f:
     # this adds __version__ to setup.py
     exec(f.read())
+
+
+version = "0.0.0"
+changes = os.path.join(here, "CHANGES.rst")
+pattern = r'^(?P<version>[0-9]+.[0-9]+(.[0-9]+)?)'
+with codecs.open(changes, encoding='utf-8') as changes:
+    for line in changes:
+        match = re.match(pattern, line)
+        if match:
+            version = match.group("version")
+            break
 
 
 class VersionCommand(Command):
@@ -26,7 +38,7 @@ class VersionCommand(Command):
         pass
 
     def run(self):
-        print(__version__)  # NOQA
+        print(version)  # NOQA
 
 
 with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
@@ -52,7 +64,7 @@ install_requirements = ['boto3>=1.3.0',
 
 setup(
     name="olist-loafer",
-    version=__version__,  # NOQA
+    version=version,  # NOQA
     description='Asynchronous message dispatcher for concurrent tasks processing',
     long_description=long_description,
     url='https://github.com/solidarium/loafer/',
@@ -76,5 +88,7 @@ setup(
     setup_requires=['pytest-runner'],
     install_requires=install_requirements,
     tests_require=tests_requirements,
-    cmdclass={'version': VersionCommand},
+    cmdclass={
+        'version': VersionCommand
+    },
 )
