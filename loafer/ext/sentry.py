2f1758eabb4544616865c3d6c2a981108eee223b
Christian Hartung <christian.hartung@olist.com>
2020-05-26 13:53:38 -0300

    Add support for sentry_sdk

--- loafer/ext/sentry.py
+++ loafer/ext/sentry.py
@@ -1,14 +1,12 @@
 # TODO: it should be async
-# https://github.com/getsentry/raven-aiohttp
 
 
-def sentry_handler(client, delete_message=False):
+def sentry_handler(sdk_or_hub, delete_message=False):
 
     def send_to_sentry(exc_info, message):
-        client.captureException(
-            exc_info,
-            extra={'message': message},
-        )
+        with sdk_or_hub.push_scope() as scope:
+            scope.set_extra("message", message)
+            sdk_or_hub.capture_exception(exc_info)
         return delete_message
 
     return send_to_sentry
