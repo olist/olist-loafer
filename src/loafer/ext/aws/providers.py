a04d1068583c89e57e72c93949abc761912d7ef0
Christian Hartung <christian.hartung@olist.com>
2025-02-26 18:44:49 -0300

    refactor: remove SQS backoff factor

--- src/loafer/ext/aws/providers.py
+++ src/loafer/ext/aws/providers.py
@@ -5,7 +5,6 @@ import botocore.exceptions
 
 from loafer.exceptions import ProviderError
 from loafer.providers import AbstractProvider
-from loafer.utils import calculate_backoff_multiplier
 
 from .bases import BaseSQSClient
 
@@ -16,13 +15,6 @@ class SQSProvider(AbstractProvider, BaseSQSClient):
     def __init__(self, queue_name, options=None, **kwargs):
         self.queue_name = queue_name
         self._options = options or {}
-        self._backoff_factor = self._options.pop("BackoffFactor", None)
-        if self._backoff_factor is not None:
-            attributes_names = self._options.get("AttributeNames", [])
-            if "ApproximateReceiveCount" not in attributes_names and "All" not in attributes_names:
-                attributes_names.append("ApproximateReceiveCount")
-                self._options["AttributeNames"] = attributes_names
-
         super().__init__(**kwargs)
 
     def __str__(self):
@@ -41,31 +33,6 @@ class SQSProvider(AbstractProvider, BaseSQSClient):
                 return True
 
             raise
-
-    async def message_not_processed(self, message):
-        receipt = message["ReceiptHandle"]
-        if self._backoff_factor:
-            backoff_multiplier = calculate_backoff_multiplier(
-                int(message["Attributes"]["ApproximateReceiveCount"]),
-                self._backoff_factor,
-            )
-
-            custom_visibility_timeout = round(backoff_multiplier * self._options.get("VisibilityTimeout", 30))
-            logger.info(
-                "message not processed, receipt=%r, custom_visibility_timeout=%r", receipt, custom_visibility_timeout
-            )
-            queue_url = await self.get_queue_url(self.queue_name)
-            try:
-                async with self.get_client() as client:
-                    return await client.change_message_visibility(
-                        QueueUrl=queue_url,
-                        ReceiptHandle=receipt,
-                        VisibilityTimeout=custom_visibility_timeout,
-                    )
-            except botocore.exceptions.ClientError as exc:
-                if "InvalidParameterValue" not in str(exc):
-                    raise
-        return None
 
     async def fetch_messages(self):
         logger.debug("fetching messages on %s", self.queue_name)
--- src/loafer/utils.py
+++ src/loafer/utils.py
@@ -2,7 +2,7 @@ import logging
 
 from ._compat import iscoroutinefunction, to_thread
 
-logger = logging.getLogger(__name__)
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 async def ensure_coroutinefunction(func, *args):
@@ -12,7 +12,3 @@ async def ensure_coroutinefunction(func, *args):
 
     logger.debug("handler will run in a separate thread: %r", func)
     return await to_thread(func, *args)
-
-
-def calculate_backoff_multiplier(number_of_tries, backoff_factor):
-    return backoff_factor**number_of_tries
