2b70f2f0267753250789a7e6ed623ba1e29cefc4
Christian Hartung <christian.hartung@olist.com>
2025-02-27 09:15:24 -0300

    fix: make sync handlers respect contextvars

--- src/loafer/_compat.py
+++ src/loafer/_compat.py
@@ -12,14 +12,17 @@ if sys.version_info >= (3, 11):
     ExceptionGroup = builtins.ExceptionGroup
 else:
     import asyncio
+    import contextvars
     import functools
 
     from exceptiongroup import ExceptionGroup
     from taskgroup import TaskGroup
 
     async def to_thread(func, /, *args, **kwargs):
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, functools.partial(func, *args, **kwargs))
+        loop = asyncio.get_running_loop()
+        ctx = contextvars.copy_context()
+        func_call = functools.partial(ctx.run, func, *args, **kwargs)
+        return await loop.run_in_executor(None, func_call)
 
 
 __all__ = [
